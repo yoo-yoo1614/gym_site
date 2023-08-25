649e8a2ec3bcdb7020108a986e26d612bbb0ad69
yoo-yoo1614 <harshit.dms@gmail.com>
2023-08-26 01:05:12 +0530

    second commit

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -8,7 +8,7 @@ const HeroBanner = () => (
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Eat, Sleep <br />
-      Lift, Repeat
+      Lift, 
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
       Check out the most effective exercises personalized to you
