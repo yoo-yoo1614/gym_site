1fbf8ccc5c4ea9febe79d349b87909e3923cc40b
yoo-yoo1614 <harshit.dms@gmail.com>
2024-06-19 20:16:31 +0530

    API update

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,22 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+      'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+      'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
     }
   };
 
   export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'e0bb6a25e0msh0f59699454a01b9p116b26jsnba927bd389d7',
+      'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
 
 export const fetchData = async (url, options) => {
-  const res = await fetch(url, options);
-  const data = await res.json();
+  const response = await fetch(url, options);
+  const data = await response.json();
 
   return data;
-};
+}
